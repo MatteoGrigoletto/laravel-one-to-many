bd693d509cb598af8679c4106471cd6d4e814617
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-01-30 14:14:40 +0100

    Inizio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,10 +18,4 @@
         <button class="btn btn-primary btn-lg" type="button">Example button</button>
     </div>
 </div>
-
-<div class="content">
-    <div class="container">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p>
-    </div>
-</div>
 @endsection
